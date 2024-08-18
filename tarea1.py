9b54e1f7dc41ce3d97b699327c92659d6e40ee01
GerardoHuHer <gerardohuerta1502@gmail.com>
2024-08-17 21:41:56 -0600

    Tarea 1 modificaciones

--- tarea1.py
+++ tarea1.py
@@ -3,32 +3,42 @@
 '''
 from PIL import Image
 
-def imagenNegativo(path: str):
+def imagenNegativo(path: str) -> str:
     '''
         Función que recibe el path de la imagen y devuelve su negativo
     '''
-    img = Image.open(path)
-    img.show()
-    img2 = img.copy()
-    pixels = list(img2.getdata())
-    negativo = list(map(lambda pixel: (255 - pixel[0], 255 - pixel[1], 255 - pixel[2]), pixels))
-    img2.putdata(negativo)
-    img2.show()
+    try:        
+        img = Image.open(path)
+        img.show()
+        img2 = img.copy()
+        pixels = list(img2.getdata())
+        negativo = list(map(lambda pixel: (255 - pixel[0], 255 - pixel[1], 255 - pixel[2]), pixels))
+        img2.putdata(negativo)
+        img2.show()
+        return "Se ha creado el negativo de la foto."
+    except:
+        return "Fallo al realizar las modificaciones a la foto."
 
-def blancoNegro(path: str):
+    
+
+def blancoNegro(path: str) -> str:
     '''
         Función para pasar a escala de grises una imagen
     '''
-    img = Image.open(path)
-    img.show()
-    img2 = img.copy()
-    img2 = img2.convert("L")
-    pixels = list(img2.getdata())
-    grises = list(map(lambda pixel: (255 - pixel), pixels))
-    img2.putdata(grises)
-    img2.show()
+    try:
+        img = Image.open(path)
+        img.show()
+        img2 = img.copy()
+        img2 = img2.convert("L")
+        pixels = list(img2.getdata())
+        grises = list(map(lambda pixel: (255 - pixel), pixels))
+        img2.putdata(grises)
+        img2.show()
+        return "La foto se ha modificado correctamente"
+    except:
+        return "Fallo al modificar la foto."
 
-imagenNegativo("/home/gd_15/Descargas/obsidian.png")
-blancoNegro("/home/gd_15/Descargas/obsidian.png")
+print(imagenNegativo("/home/gd_15/Descargas/obsidian.png"))
+print(blancoNegro("/home/gd_15/Descargas/obsidian.png"))
 
 
